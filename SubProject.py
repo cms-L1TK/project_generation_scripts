51e37749c07edd436a23fc18277f4ed4637716d5
Anders Ryd <aryd@cern.ch>
2015-11-04 00:07:50 -0500

    Script to generate sub project

---
+++ SubProject.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import math
+import sys
+
+#Config D5D6
+#regions = ["D5","D6"] 
+#vregions = ["D1","D2","D3","D4","D7","D8","L1L2","F1L","_L1","_L2"]
+
+#Config D3
+regions = ["D3"] 
+vregions = ["D1","D2","D4","D5","D6","D7","D8","_F1","_F2","_F3","_F4","_F5"]
+
+#Config D4
+#regions = ["D4"] 
+#vregions = ["D1","D2","D3","D5","D6","D7","D8","_F1","_F2","_F3","_F4","_F5"]
+
+#Config D3+D4
+#regions = ["D3","D4"] 
+#vregions = ["D1","D2","D5","D6","D7","D8","_F1","_F2","_F3","_F4","_F5"]
+
+
+print 'Will read wires.input.fullsector and select ',regions
+ 
+
+fi = open("wires.input.fullsector","r")
+
+fo = open("wires.reduced","w")
+
+for line in fi:
+    match=False
+    for region in regions:
+        if region in line:
+            match=True;
+    if not match:
+        continue
+    substr=line.split()
+    countgt=0
+    inputmem=[]
+    outputmem=[]
+    cmd=""
+    for str in substr:
+        if str == ">" :
+            countgt+=1
+            continue
+        if countgt==1 :
+            cmd=str
+            continue
+        veto=False
+        for vregion in vregions:
+            if vregion in str:
+                if not "SD" in str:
+                    veto=True;
+        if not veto:
+            if countgt==0:
+                inputmem.append(str)
+            if countgt==2:
+                outputmem.append(str)
+                
+    if len(inputmem)!=0:
+        if len(outputmem)!=0:
+            for str in inputmem:
+                fo.write(str+" ")
+            fo.write(" > "+cmd+" >")
+            for str in outputmem:
+                fo.write(" "+str)
+            fo.write("\n")
+
+
+
+

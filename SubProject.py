2446488ba342740f68e2bd60987a896bc4abd013
mez34 <mez34@cornell.edu>
2016-08-03 15:16:58 +0200

    removed extra D5 defintion in SubProject.py

--- SubProject.py
+++ SubProject.py
@@ -4,7 +4,7 @@ import math
 import sys
 
 if (len(sys.argv) != 2 ) :
-    print "Usage: SubProject.py wires.D3|D4|D3D4|D5D6"
+    print "Usage: SubProject.py wires.D3|D4|D3D4|D5|D5D6"
     exit(1)
 
 print 'Will process', sys.argv[1]
@@ -31,10 +31,6 @@ if sys.argv[1]=="D3":
 if sys.argv[1]=="D4":
     regions = ["D4"] 
     vregions = ["D1","D2","D3","D5","D6","D7","D8","_F1","_F2","_F3","_F4","_F5"]
-
-if sys.argv[1]=="D5":
-    regions = ["D5"] 
-    vregions = ["D1","D2","D3","D4","D6","D7","D8","L1L2","F1L","_L1","_L2","_L3","_L4","_L5","_L6","_B1","_B2"]
 
 if sys.argv[1]=="D3D4":
     regions = ["D3","D4"] 

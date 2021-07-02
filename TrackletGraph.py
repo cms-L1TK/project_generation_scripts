079b0aeb32493b28ff744c31a4aed0b368ec044d
meisonlikesicecream <56160327+meisonlikesicecream@users.noreply.github.com>
2021-07-02 19:47:27 +0100

    Add nentries ports for allstub and allproj if end of chain (#28)
    * added nentries ports for allstub and allproj if end of chain for vhdl debugging purposes
    
    * added comment

--- TrackletGraph.py
+++ TrackletGraph.py
@@ -225,9 +225,10 @@ class TrackletGraph(object):
     def populate_has_numEntries_out(mem,hls_dir):
         # Some memories need no numEntries out port, as no processing module wants to read it.
         # (Check which by searching for GetEntries() in HLS code).
-        if mem.mtype == "AllStubs":
+        # If those memories are at the of the chain, add numEntries port anyway for VHDL chain debugging purposes.
+        if mem.mtype == "AllStubs" and not mem.is_final:
             mem.has_numEntries_out = False
-        elif mem.mtype == "AllProj":
+        elif mem.mtype == "AllProj" and not mem.is_final:
             mem.has_numEntries_out = False
         else:
             mem.has_numEntries_out = True

0b2adfc3d40c255820884d2232cbee0d73e2ba6b
Peter Wittich <wittich@cornell.edu>
2021-07-08 10:21:06 -0500

    Update generator_hdl.py

--- generator_hdl.py
+++ generator_hdl.py
@@ -7,6 +7,12 @@
 ################################################
 from __future__ import absolute_import
 from __future__ import print_function
+
+try:
+    from rich.traceback import install
+    install()
+except:
+    pass
 from TrackletGraph import MemModule, ProcModule, MemTypeInfoByKey, TrackletGraph
 from WriteHDLUtils import arrangeMemoriesByKey, \
                             writeModuleInstance
@@ -67,6 +73,7 @@ def writeProcModules(proc_list, hls_src_dir, extraports):
     proc_type_list = []
 
     for aProcMod in proc_list:
+        print("procname"+ aProcMod.inst)
         if not aProcMod.mtype in proc_type_list: # Is this aProcMod the first of its type
             proc_wire_inst,proc_func_inst = writeModuleInstance(aProcMod, hls_src_dir, True, extraports)
             proc_type_list.append(aProcMod.mtype)

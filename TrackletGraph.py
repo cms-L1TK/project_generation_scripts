c009da7daa2918ca5849d25a5a8b10e65e207a0c
Andrew Hart <ahart@cern.ch>
2023-12-12 07:10:30 -0500

    Updated widths of TPAR and TW.

--- TrackletGraph.py
+++ TrackletGraph.py
@@ -214,7 +214,7 @@ class TrackletGraph(object):
         elif mem.mtype == "StubPairs":
             mem.bitwidth = 14
         elif mem.mtype == "TrackletParameters":
-            mem.bitwidth = 70
+            mem.bitwidth = 73
         elif mem.mtype == "TrackletProjections" or mem.mtype == "AllProj":
             if barrelPS>-1: mem.bitwidth = 60
             if barrel2S>-1: mem.bitwidth = 58
@@ -231,7 +231,7 @@ class TrackletGraph(object):
             if barrelPS>-1 or barrel2S>-1: mem.bitwidth = 52
             if disk>-1: mem.bitwidth = 55
         elif mem.mtype == "TrackWord":
-            mem.bitwidth = 98
+            mem.bitwidth = 104
         elif mem.mtype == "BarrelStubWord":
             mem.bitwidth = 46
         elif mem.mtype == "DiskStubWord":
